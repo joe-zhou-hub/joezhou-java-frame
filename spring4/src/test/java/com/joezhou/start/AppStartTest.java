b94314a62c8491c989a2ac9261d661435e147b1c
joezhou <yy06200210@163.com>
2020-12-20 13:16:11 +0800

    app-multiple

--- spring4/src/test/java/com/joezhou/start/AppStartTest.java
+++ spring4/src/test/java/com/joezhou/start/AppStartTest.java
@@ -18,4 +18,28 @@ public class AppStartTest {
         System.out.println(student.getName());
         app.close();
     }
+
+    @Test
+    public void multipleByConstructor() {
+        ClassPathXmlApplicationContext app = new ClassPathXmlApplicationContext("spring/start/app-multiple-a.xml", "spring/start/app-multiple-b.xml");
+        Student studentA = (Student) app.getBean("studentA");
+        Student studentB = (Student) app.getBean("studentB");
+        studentA.setName("studentA");
+        System.out.println(studentA.getName());
+        studentB.setName("studentB");
+        System.out.println(studentB.getName());
+        app.close();
+    }
+
+    @Test
+    public void multipleByImport() {
+        ClassPathXmlApplicationContext app = new ClassPathXmlApplicationContext("spring/start/app-multiple-c.xml");
+        Student studentC = (Student) app.getBean("studentC");
+        Student studentD = (Student) app.getBean("studentD");
+        studentC.setName("studentC");
+        System.out.println(studentC.getName());
+        studentD.setName("studentD");
+        System.out.println(studentD.getName());
+        app.close();
+    }
 }
