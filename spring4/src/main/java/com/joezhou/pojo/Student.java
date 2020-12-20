23e63a4782c3784ffbc38655f830db791a32a49d
joezhou <yy06200210@163.com>
2020-12-20 11:18:54 +0800

    app-start

---
+++ spring4/src/main/java/com/joezhou/pojo/Student.java
@@ -0,0 +1,13 @@
+package com.joezhou.pojo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author JoeZhou
+ */
+@Data
+public class Student implements Serializable {
+    private String name;
+}
---
+++ spring4/src/test/java/com/joezhou/start/AppStartTest.java
@@ -0,0 +1,23 @@
+package com.joezhou.start;
+
+import com.joezhou.pojo.Student;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author JoeZhou
+ */
+public class AppStartTest {
+
+    @Test
+    public void start() {
+        // "classpath:" 可以省略不写。
+        ClassPathXmlApplicationContext app =
+                new ClassPathXmlApplicationContext("classpath:spring/start/app-start.xml");
+
+        Student student = (Student) app.getBean("student");
+        student.setName("赵四");
+        System.out.println(student.getName());
+        app.close();
+    }
+}
