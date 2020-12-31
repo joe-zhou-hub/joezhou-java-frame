97811e9e06ed6e14c23344fef504a509c8573042
joezhou <yy06200210@163.com>
2020-12-31 14:00:49 +0800

    属性文件

--- mybatis3/src/test/java/com/joezhou/start/StudentTest.java
+++ mybatis3/src/test/java/com/joezhou/start/StudentTest.java
@@ -7,8 +7,10 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * @author JoeZhou
@@ -32,6 +34,29 @@ public class StudentTest {
         System.out.println(MyBatisUtil.getFactory("mybatis-student.xml").hashCode());
         System.out.println(MyBatisUtil.getFactory("mybatis-student.xml").hashCode());
         try (SqlSession session = factory.openSession()) {
+            System.out.println(session.getConnection().isClosed());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void useProperties() {
+        SqlSessionFactory factory = MyBatisUtil.getFactory("mybatis-student.xml");
+        try (SqlSession session = factory.openSession()) {
+            System.out.println(session.getConnection().isClosed());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void usePropertiesInFactory() throws Exception {
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("src/main/resources/jdbc/db.properties"));
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(
+                Resources.getResourceAsStream("mybatis-student.xml"), properties);
+        try(SqlSession session = factory.openSession()) {
             System.out.println(session.getConnection().isClosed());
         } catch (Exception e) {
             e.printStackTrace();
