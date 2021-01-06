7522aefa0cc9c8de65379dc834453ca3dee77fe0
joezhou <yy06200210@163.com>
2021-01-06 16:40:08 +0800

    SM整合包

--- mybatis3/src/main/java/com/joezhou/dao/impl/DogDaoImpl.java
+++ mybatis3/src/main/java/com/joezhou/dao/impl/DogDaoImpl.java
@@ -2,7 +2,6 @@ package com.joezhou.dao.impl;
 
 import com.joezhou.dao.DogDao;
 import com.joezhou.pojo.Dog;
-import org.apache.ibatis.session.SqlSession;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 
 /**
@@ -12,7 +11,6 @@ public class DogDaoImpl extends SqlSessionDaoSupport implements DogDao {
 
     @Override
     public void insert(Dog dog) {
-        SqlSession session = this.getSqlSession();
-        session.insert("dogSpace.insert", dog);
+        getSqlSession().insert("dogSpace.insert", dog);
     }
 }
