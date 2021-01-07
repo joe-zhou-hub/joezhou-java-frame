81539d06343405f3e7fc86c25e682135326c90a0
joezhou <yy06200210@163.com>
2021-01-07 12:06:45 +0800

    SM整合 接口开发

---
+++ mybatis3-generator/src/main/java/com/joezhou/app/Generator.java
@@ -0,0 +1,26 @@
+package com.joezhou.app;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author JoeZhou
+ */
+public class Generator {
+    public static void main(String[] args) throws Exception {
+        List<String> warnings = new ArrayList<>();
+        ConfigurationParser parser = new ConfigurationParser(warnings);
+        File generatorConfigPath = new File("generator.xml");
+        Configuration config = parser.parseConfiguration(generatorConfigPath);
+        DefaultShellCallback callback = new DefaultShellCallback(true);
+        MyBatisGenerator generator = new MyBatisGenerator(config, callback, warnings);
+        generator.generate(null);
+        System.out.println("生成结束！");
+    }
+}
