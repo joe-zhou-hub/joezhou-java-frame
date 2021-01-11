2e8120ef338cd41132a39b85343f1ffdf127d0bf
joezhou <yy06200210@163.com>
2021-01-11 11:03:38 +0800

    mybatis逆向工程

---
+++ mybatis3-generator/src/main/java/com/joezhou/pojo/User.java
@@ -0,0 +1,73 @@
+package com.joezhou.pojo;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private Integer id;
+
+    private String name;
+
+    private Boolean gender;
+
+    private Integer age;
+
+    private String info;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Boolean getGender() {
+        return gender;
+    }
+
+    public void setGender(Boolean gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info == null ? null : info.trim();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", gender=").append(gender);
+        sb.append(", age=").append(age);
+        sb.append(", info=").append(info);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
