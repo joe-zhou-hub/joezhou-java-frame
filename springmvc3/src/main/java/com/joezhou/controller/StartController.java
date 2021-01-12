47edf53e38c00d8143650fa35063eff303e05116
joezhou <yy06200210@163.com>
2021-01-12 15:53:58 +0800

    springmvc配通

---
+++ springmvc3/src/main/java/com/joezhou/controller/StartController.java
@@ -0,0 +1,19 @@
+package com.joezhou.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author JoeZhou
+ */
+
+@Controller
+public class StartController {
+
+    @RequestMapping("/api/start/start.action")
+    public String start() {
+        System.out.println("start()...");
+        return "success";
+    }
+
+}
