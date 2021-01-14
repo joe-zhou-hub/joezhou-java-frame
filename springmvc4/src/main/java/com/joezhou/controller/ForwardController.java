e0c46fcc357102995c790c8024446291d67026e8
joezhou <yy06200210@163.com>
2021-01-14 10:43:04 +0800

    springmvc原生跳页

--- springmvc4/src/main/java/com/joezhou/controller/ForwardController.java
+++ springmvc4/src/main/java/com/joezhou/controller/ForwardController.java
@@ -17,19 +17,34 @@ public class ForwardController {
 
     @RequestMapping("request-forward")
     public void requestForward(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("requestForward()...");
         req.getRequestDispatcher("/view/success.html").forward(req, resp);
     }
 
     @RequestMapping("response-redirect")
     public void responseRedirect(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        System.out.println("responseRedirect()...");
         resp.setContentType("text/html;charset=utf-8");
         resp.sendRedirect(req.getContextPath() + "/view/success.html");
     }
 
     @RequestMapping("response-writer")
     public void responseWriter(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        System.out.println("responseWriter()...");
         resp.setContentType("application/json;charset=utf-8");
         resp.getWriter().print("response-writer");
+    }
+
+    @RequestMapping("string-forward")
+    public String stringForward() {
+        System.out.println("stringForward()...");
+        return "forward:string-redirect";
+    }
+
+    @RequestMapping("string-redirect")
+    public String stringRedirect() {
+        System.out.println("stringRedirect()...");
+        return "redirect:/view/success.html";
     }
 
 }
