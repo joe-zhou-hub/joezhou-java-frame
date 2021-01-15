5c440f2752873587bbb0b303b95f0cb0659a7170
joezhou <yy06200210@163.com>
2021-01-15 11:50:50 +0800

    springmvc download

---
+++ springmvc4/src/main/java/com/joezhou/controller/InterceptorController.java
@@ -0,0 +1,20 @@
+package com.joezhou.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author JoeZhou
+ */
+@Controller
+@RequestMapping("/api/interceptor")
+public class InterceptorController {
+
+    @ResponseBody
+    @RequestMapping("test")
+    public String test() {
+        System.out.println("test()...");
+        return "ok";
+    }
+}
---
+++ springmvc4/src/main/java/com/joezhou/interceptor/MyInterceptorA.java
@@ -0,0 +1,29 @@
+package com.joezhou.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author JoeZhou
+ */
+public class MyInterceptorA implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+        System.out.println("MyInterceptorA:preHandle()...");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest req, HttpServletResponse resp,Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("MyInterceptorA:postHandle()...");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest req, HttpServletResponse resp, Object handler, Exception ex) throws Exception {
+        System.out.println("MyInterceptorA:afterCompletion()...");
+    }
+}
