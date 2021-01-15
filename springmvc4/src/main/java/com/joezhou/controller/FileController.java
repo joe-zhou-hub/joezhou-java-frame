80934f91728d703b85a6088c0bd11525ef1eccd2
joezhou <yy06200210@163.com>
2021-01-15 11:31:28 +0800

    springmvc download

--- springmvc4/src/main/java/com/joezhou/controller/FileController.java
+++ springmvc4/src/main/java/com/joezhou/controller/FileController.java
@@ -1,5 +1,10 @@
 package com.joezhou.controller;
 
+import org.apache.commons.io.FileUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,5 +31,14 @@ public class FileController {
         }
         avatar.transferTo(desc);
         return "ok";
+    }
+
+    @RequestMapping("download")
+    public ResponseEntity<byte[]> download(String file) throws IOException {
+        byte[] bytes = FileUtils.readFileToByteArray(new File("F:\\upload\\" + file));
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentDispositionFormData("attachment", file);
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        return new ResponseEntity<>(bytes, headers, HttpStatus.CREATED);
     }
 }
