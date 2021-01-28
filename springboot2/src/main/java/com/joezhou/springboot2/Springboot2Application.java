9431feb26b1ade5cd39ad16c7bd41bdceaca89d1
joezhou <yy06200210@163.com>
2021-01-28 11:14:00 +0800

    springboot 自动搭建

---
+++ springboot2/src/main/java/com/joezhou/springboot2/Springboot2Application.java
@@ -0,0 +1,16 @@
+package com.joezhou.springboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JoeZhou
+ */
+@SpringBootApplication
+public class Springboot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2Application.class, args);
+    }
+
+}
