d835e5e082c1f8327302f87b1fb73c874d48514c
joezhou <yy06200210@163.com>
2021-01-29 14:26:19 +0800

    springboot logback

---
+++ springboot2/src/main/java/com/joezhou/springboot2/controller/LogBackController.java
@@ -0,0 +1,26 @@
+package com.joezhou.springboot2.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author JoeZhou
+ */
+@RestController
+@RequestMapping("api/logback")
+public class LogBackController {
+
+    private Logger logger = LoggerFactory.getLogger(LogBackController.class);
+
+    @RequestMapping("test")
+    public String test() {
+        logger.debug("DEBUG级别信息...");
+        logger.info("INFO级别信息...");
+        logger.warn("WARN级别信息...");
+        logger.error("ERROR级别信息...");
+        return "success";
+    }
+
+}
