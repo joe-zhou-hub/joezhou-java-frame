9115cc3e4b87a5fed1a879f80b3a8b209cbf601c
joezhou <yy06200210@163.com>
2021-01-29 09:37:19 +0800

    springboot starter

---
+++ springboot2/src/test/java/com/joezhou/springboot2/ShieldTest.java
@@ -0,0 +1,16 @@
+package com.joezhou.springboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author JoeZhou
+ */
+class ShieldTest {
+
+    @Test
+    void shield() {
+        String baseUrl = "http://127.0.0.1:8080/api/shield/test?word=a-blue-apple";
+        System.out.println(new RestTemplate().getForObject(baseUrl, String.class));
+    }
+}
