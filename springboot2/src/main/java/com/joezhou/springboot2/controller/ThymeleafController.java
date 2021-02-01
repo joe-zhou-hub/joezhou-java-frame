c0257264f17a8d7bd04dbd37eac48ea0e1a837c2
joezhou <yy06200210@163.com>
2021-02-01 09:38:42 +0800

    springboot thymeleaf

---
+++ springboot2/src/main/java/com/joezhou/springboot2/controller/ThymeleafController.java
@@ -0,0 +1,21 @@
+package com.joezhou.springboot2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+/**
+ * @author JoeZhou
+ */
+@Controller
+@RequestMapping("api/thymeleaf")
+public class ThymeleafController {
+
+    @RequestMapping("test")
+    public String test(Map<String, Object> map, String name) {
+        map.put("msg", name);
+        // 搭配前缀后缀，组成/templates/html/main.html
+        return "main";
+    }
+}
