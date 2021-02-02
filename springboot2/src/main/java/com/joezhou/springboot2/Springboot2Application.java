9251c6c5b3256f776f07e1d87a3ccffa94ff320f
joezhou <yy06200210@163.com>
2021-02-02 10:51:53 +0800

    springboot 独立部署

--- springboot2/src/main/java/com/joezhou/springboot2/Springboot2Application.java
+++ springboot2/src/main/java/com/joezhou/springboot2/Springboot2Application.java
@@ -1,24 +1,26 @@
 package com.joezhou.springboot2;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
 
 /**
  * @author JoeZhou
  */
-/*@SpringBootApplication(exclude = SolrAutoConfiguration.class)
+@SpringBootApplication(exclude = SolrAutoConfiguration.class)
 @MapperScan("com.joezhou.springboot2.mybatis.mapper")
 public class Springboot2Application {
     public static void main(String[] args) {
         SpringApplication.run(Springboot2Application.class, args);
     }
-}*/
+}
 
+/*
 @SpringBootApplication
 public class Springboot2Application extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(Springboot2Application.class);
     }
-}
+}*/
