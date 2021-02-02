5ed3b71a2db5addc9c601fe2d6b5faad63d7bfa0
joezhou <yy06200210@163.com>
2021-02-02 16:09:12 +0800

    springboot websocket

---
+++ springboot2/src/main/java/com/joezhou/springboot2/sse/SseController.java
@@ -0,0 +1,22 @@
+package com.joezhou.springboot2.sse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author JoeZhou
+ */
+@RestController
+@RequestMapping("api/sse")
+public class SseController {
+
+    @RequestMapping(value = "test", produces = "text/event-stream;charset=UTF-8")
+    public String test() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "data:" + Math.random() + "\n\n";
+    }
+}
