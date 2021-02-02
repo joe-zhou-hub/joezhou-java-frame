101d7bb3488433afa19f9ed39817d7ec2922b731
joezhou <yy06200210@163.com>
2021-02-02 15:38:04 +0800

    springboot websocket

--- springboot2/src/main/java/com/joezhou/springboot2/websocket/WebSocketServer.java
+++ springboot2/src/main/java/com/joezhou/springboot2/websocket/WebSocketServer.java
@@ -6,7 +6,6 @@ import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -16,55 +15,45 @@ import java.util.concurrent.ConcurrentHashMap;
 @Component
 public class WebSocketServer {
 
-    private static Map<String, WebSocketServer> webSocketServerMap = new ConcurrentHashMap<>();
+    /**
+     * 用于存储每个BS用户的WebSocketServer实例
+     * ConcurrentHashMap保证线程安全，static保证实例唯一
+     */
+    private static Map<String, WebSocketServer> servers = new ConcurrentHashMap<>();
     private Session session;
 
     @OnOpen
     public void onOpen(@PathParam("id") String id, Session session) {
-
-        // 后面推送消息的时候还会用到session对象
         this.session = session;
-
-        webSocketServerMap.put(id, this);
-
-        // session.getRequestParameterMap();
-        // 向B端的onmessage()事件中传递消息："连接成功..."
-        session.getAsyncRemote().sendText("连接成功...");
-        System.out.println("用户连入：" + id);
-        System.out.println("当前连接总数：" + webSocketServerMap.size());
+        servers.put(id, this);
+        for (String key : servers.keySet()) {
+            servers.get(key).session.getAsyncRemote().sendText("cli-" + id + " login...");
+        }
+        System.out.println("cli-" + id + " login...");
+        System.out.println("current clients size：" + servers.size());
     }
 
     @OnClose
     public void onClose(@PathParam("id") String id, Session session) {
-        webSocketServerMap.remove(id);
-        System.out.println("用户登出：" + id);
-        System.out.println("当前连接总数：" + webSocketServerMap.size());
-    }
+        servers.remove(id);
+        System.out.println("cli-" + id + " logoff...");
+        System.out.println("current clients size：" + servers.size());
 
-    @OnError
-    public void onError(Throwable e, Session session) {
-        System.out.println("发生错误！");
-        e.printStackTrace();
+        for (String key : servers.keySet()) {
+            servers.get(key).session.getAsyncRemote().sendText("cli-" + id + " logoff...");
+        }
     }
 
     @OnMessage
-    public void onMessage(String msg, Session session) {
-        System.out.println("来自客户端的消息:" + msg);
+    public void onMessage(@PathParam("id") String id, String msg, Session session) {
+        for (String key : servers.keySet()) {
+            servers.get(key).session.getAsyncRemote().sendText("from cli-" + id + ": " + msg);
+        }
     }
 
-    public void sendById(String msg, String id) {
-        // zhaosi 001
-        WebSocketServer webSocketServer = webSocketServerMap.get(id);
-        if (webSocketServer != null) {
-            webSocketServer.session.getAsyncRemote().sendText(msg);
-        }
-    }
-    
-    public void sendToAll(String msg) {
-        Set<String> webSocketServerKeySet = webSocketServerMap.keySet();
-        for (String key : webSocketServerKeySet) {
-            WebSocketServer webSocketServer = webSocketServerMap.get(key);
-            webSocketServer.session.getAsyncRemote().sendText(msg);
-        }
+    @OnError
+    public void onError(@PathParam("id") String id, Throwable e, Session session) {
+        System.out.println("cli-" + id + " error...");
+        e.printStackTrace();
     }
 }
