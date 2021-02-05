d62bbd929d1af468cb62dd5027a3de5eea35c00e
joezhou <yy06200210@163.com>
2021-02-05 11:23:07 +0800

    springboot webflux

---
+++ springboot2-webflux/src/main/java/com/joezhou/springboot2webflux/Springboot2WebfluxApplication.java
@@ -0,0 +1,17 @@
+package com.joezhou.springboot2webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JoeZhou
+ */
+@SpringBootApplication
+public class Springboot2WebfluxApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Springboot2WebfluxApplication.class, args);
+	}
+
+}
---
+++ springboot2-webflux/src/test/java/com/joezhou/springboot2webflux/Springboot2WebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joezhou.springboot2webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2WebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
