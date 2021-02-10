35b53e7ba87b82403e693e2561b33ecdf7f5c40f
joezhou <yy06200210@163.com>
2021-02-10 14:09:37 +0800

    springboot springboot-rocketmq

--- springboot2-rocketmq/src/main/java/com/joezhou/springboot2rocketmq/controller/RocketMqController.java
+++ springboot2-rocketmq/src/main/java/com/joezhou/springboot2rocketmq/controller/RocketMqController.java
@@ -23,10 +23,11 @@ public class RocketMqController {
     }
 
     @RequestMapping("send")
-    public void send(String topic, String tag, String msg) throws Exception {
+    public String send(String topic, String tag, String msg) throws Exception {
         SendResult result = producerA.getProducer().send(
                 new Message(topic, tag, msg.getBytes(RemotingHelper.DEFAULT_CHARSET)));
         System.out.println("msg-id: " + result.getMsgId());
         System.out.println("status: " + result.getSendStatus());
+        return "success";
     }
 }
