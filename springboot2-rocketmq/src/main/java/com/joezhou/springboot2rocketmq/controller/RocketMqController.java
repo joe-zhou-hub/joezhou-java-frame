90e0e8a9239187cfdfbb26b8feb5e1ce380eeee4
joezhou <yy06200210@163.com>
2021-02-10 14:09:24 +0800

    springboot springboot-rocketmq

--- springboot2-rocketmq/src/main/java/com/joezhou/springboot2rocketmq/controller/RocketMqController.java
+++ springboot2-rocketmq/src/main/java/com/joezhou/springboot2rocketmq/controller/RocketMqController.java
@@ -1,17 +1,12 @@
 package com.joezhou.springboot2rocketmq.controller;
 
 import com.joezhou.springboot2rocketmq.producer.ProducerA;
-import org.apache.rocketmq.client.exception.MQBrokerException;
-import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
-import org.apache.rocketmq.remoting.exception.RemotingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.UnsupportedEncodingException;
 
 /**
  * @author JoeZhou
@@ -28,11 +23,9 @@ public class RocketMqController {
     }
 
     @RequestMapping("send")
-    public void send(String topic, String tag, String msg) throws UnsupportedEncodingException, InterruptedException,
-            RemotingException, MQClientException, MQBrokerException {
-
-        Message message = new Message(topic, tag, msg.getBytes(RemotingHelper.DEFAULT_CHARSET));
-        SendResult result = producerA.getProducer().send(message);
+    public void send(String topic, String tag, String msg) throws Exception {
+        SendResult result = producerA.getProducer().send(
+                new Message(topic, tag, msg.getBytes(RemotingHelper.DEFAULT_CHARSET)));
         System.out.println("msg-id: " + result.getMsgId());
         System.out.println("status: " + result.getSendStatus());
     }
