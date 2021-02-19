3f6b4b3975d975c80b2cb235bc4a273e74193b4f
joezhou <yy06200210@163.com>
2021-02-19 15:43:55 +0800

    springboot springboot-security

--- springboot2-security/src/main/java/com/joezhou/springboot2security/controller/AuthController.java
+++ springboot2-security/src/main/java/com/joezhou/springboot2security/controller/AuthController.java
@@ -14,30 +14,35 @@ public class AuthController {
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @RequestMapping("admin")
     public String admin() {
+        System.out.println("admin()...");
         return "auth-success";
     }
 
     @PreAuthorize("hasRole('ROLE_COMM')")
     @RequestMapping("comm")
     public String comm() {
+        System.out.println("comm()...");
         return "auth-success";
     }
 
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_COMM')")
     @RequestMapping("all")
     public String all() {
+        System.out.println("all()...");
         return "auth-success";
     }
 
     @PreAuthorize("hasAuthority('select')")
     @RequestMapping("select")
     public String select() {
+        System.out.println("select()...");
         return "auth-success";
     }
 
     @PreAuthorize("hasAnyAuthority('delete','update','insert')")
     @RequestMapping("dml")
     public String dml() {
+        System.out.println("dml()...");
         return "auth-success";
     }
 }
