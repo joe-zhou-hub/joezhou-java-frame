7abe26f17af068b7c950a38973c53b51587e9ebc
joezhou <yy06200210@163.com>
2021-03-09 22:08:22 +0800

    springdata-redis 整合Jedis

--- springdata2-redis/src/main/java/com/joezhou/springdata2redis/config/JedisClusterConfig.java
+++ springdata2-redis/src/main/java/com/joezhou/springdata2redis/config/JedisClusterConfig.java
@@ -4,27 +4,25 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
 import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author JoeZhou
  */
 @Configuration
-@PropertySource("classpath:jedis-pool.properties")
-public class JedisConfig {
+@PropertySource({"classpath:jedis-pool.properties", "classpath:jedis-cluster.properties"})
+public class JedisClusterConfig {
 
-    @Value("${spring.redis.host}")
-    private String host;
-
-    @Value("${spring.redis.port}")
-    private int port;
+    @Value("${spring.redis.cluster.nodes}")
+    private String clusterNodes;
 
     @Value("${spring.redis.timeout}")
     private int timeout;
-
-    @Value("${spring.redis.password}")
-    private String password;
 
     @Value("${spring.redis.jedis.pool.max-idle}")
     private int maxIdle;
@@ -39,13 +37,21 @@ public class JedisConfig {
     private long maxWait;
 
     @Bean
-    public JedisPool jedisPoolFactory() {
+    public JedisCluster jedisCluster() {
         JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
         jedisPoolConfig.setMaxIdle(maxIdle);
         jedisPoolConfig.setMinIdle(minIdle);
         jedisPoolConfig.setMaxTotal(maxActive);
         jedisPoolConfig.setMaxWaitMillis(maxWait);
         jedisPoolConfig.setJmxEnabled(true);
-        return new JedisPool(jedisPoolConfig, host, port, timeout, password);
+
+        Set<HostAndPort> nodes = new HashSet<>();
+        // [[0.0.0.0, 7011], 0.0.0.0:7012, 0.0.0.0:7013, 0.0.0.0:7014, 0.0.0.0:7015, 0.0.0.0:7016]
+        String[] servers = clusterNodes.split(",");
+        for (String server : servers) {
+            String[] kv = server.split(":");
+            nodes.add(new HostAndPort(kv[0].trim(), Integer.parseInt(kv[1])));
+        }
+        return new JedisCluster(nodes, timeout, jedisPoolConfig);
     }
 }
--- springdata2-redis/src/test/java/com/joezhou/springdata2redis/jedis/JedisConfigTest.java
+++ springdata2-redis/src/test/java/com/joezhou/springdata2redis/jedis/JedisConfigTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
 import redis.clients.jedis.JedisPool;
 
 /**
@@ -15,11 +16,25 @@ class JedisConfigTest {
     @Autowired
     private JedisPool jedisPool;
 
+    @Autowired
+    private JedisCluster jedisCluster;
+
+
     @Test
-    void jedisConfig() {
+    void jedisPool() {
         try (Jedis jedis = jedisPool.getResource()) {
             jedis.setex("name", 5, "100");
             System.out.println(jedis.get("name"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void jedisCluster() {
+        try {
+            jedisCluster.setex("name", 5, "100");
+            System.out.println(jedisCluster.get("name"));
         } catch (Exception e) {
             e.printStackTrace();
         }
